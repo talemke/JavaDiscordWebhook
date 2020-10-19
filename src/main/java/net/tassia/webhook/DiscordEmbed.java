873d2fac4ea6cd3669e9fef9462ecb28764aaec2
Tassilo <tassia710@gmail.com>
2020-10-19 17:06:53 +0200

    Create DiscordEmbed.java

---
+++ src/main/java/net/tassia/webhook/DiscordEmbed.java
@@ -0,0 +1,20 @@
+package net.tassia.webhook;
+
+/**
+ * This class represents an embed.
+ */
+public class DiscordEmbed {
+
+    private String title = null;
+    private String description = null;
+    private String url = null;
+    private String timestamp = null;
+    private int color = 0;
+    private Object footer = null;
+    private Object image = null;
+    private Object thumbnail = null;
+    private Object video = null;
+    private Object author = null;
+    private Object[] fields = null;
+
+}
